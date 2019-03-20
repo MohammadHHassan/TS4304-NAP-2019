a793de3230829f598aa35b667fc9bd9ca8cee6a5
Mohammad Hj Hassan <35444902+MohammadHHassan@users.noreply.github.com>
2019-03-20 08:12:09 +0800

    Update LearningRandomNumber.java

--- day3/LearningRandomNumber.java
+++ day3/LearningRandomNumber.java
@@ -5,6 +5,7 @@ public class LearningRandomNumber
 	public static void main(String[] args)
 	{
 		int a = 1 + (int)(Math.random()*(10-1)+1);
+		// int randomNumber = min + (int)(Math.random()*((max-min)+1))
 		System.out.println("Number generated = " + a);
 	}
 }
